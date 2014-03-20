a177d31b3a4a394aa18e190248f10b34e28ec37a
alex <alex@MacBook-Pro-alex.local>
2014-03-20 23:32:47 +0400

    ready

--- app.js
+++ app.js
@@ -1,22 +1,71 @@
 'use strict';
 
-TodoApp = angular.module('Todo', []);
+var TodoApp = angular.module('Todo', []);
 
 TodoApp.controller('TodoCtrl', function($scope) {
-	$scope.todos = [];
+	
+	$scope.todos = [{
+			"desc": "test",
+			"done": false 
+		}];
+
+
+	$scope.isEmpty =false;	
+
+
+
+	$scope.checkKey =function  (evt) {
+		
+		if (evt.keyCode ==13){
+			$scope.addTodo();
+		}else{
+
+			if ($scope.isEmpty)	$scope.removeTooltip();
+		}
+	}	
 
 	$scope.addTodo = function() {
-		todos.push({
+
+		if($scope.newTodo){
+			$scope.todos.push({
 			"desc": $scope.newTodo,
 			"done": false 
 		});
+		$scope.newTodo='';
+
+
+		}else{
+			$scope.isEmpty =true;
+			setTimeout($scope.removeTooltip,500);
+
+		}
+
+
+	}
+
+	$scope.removeTooltip = function(){
+				$scope.isEmpty =false;
+				console.log("remove");	
+			}
+
+
+	$scope.removeDone=function(){
+
+		$scope.todos = _.filter($scope.todos,function (el) {
+			return !el.done 
+		});
+
 	}
 
 	$scope.do = function(index) {
-		todos[index].done = true;
+		$scope.todos[index].done = !$scope.todos[index].done;
 	}
 
 	$scope.delete = function(index) {
-		todos.splice(index, 1);
+		$scope.todos.splice(index, 1);
 	}
+	
 });
+
+
+
